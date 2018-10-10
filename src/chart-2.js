04f624946ad8d9e098bffa2292af34db77b25fb4
Yuanqi (Edward) Hong <39491376+Yuanqi-Hong@users.noreply.github.com>
2018-10-10 14:54:29 -0400

    updated charts

--- src/chart-2.js
+++ src/chart-2.js
@@ -1,8 +1,8 @@
 import * as d3 from 'd3'
 
-let margin = { top: 30, bottom: 30, left: 100, right: 100 }
-let height = 400 - margin.top - margin.bottom
-let width = 600 - margin.left - margin.right
+let margin = { top: 20, bottom: 20, left: 80, right: 80 }
+let height = 300 - margin.top - margin.bottom
+let width = 700 - margin.left - margin.right
 
 let svg = d3
   .select('#chart-2')
@@ -12,11 +12,11 @@ let svg = d3
   .append('g')
   .attr('transform', `translate(${margin.left},${margin.top})`)
 
-// let angleScale = d3.scaleBand().range([0, Math.PI * 2])
+let xPositionScale = d3.scalePoint().range([0, width])
 
 let pie = d3.pie().value(d => d.minutes)
 
-let radius = 150
+let radius = 70
 
 let arc = d3
   .arc()
@@ -30,14 +30,35 @@ d3.csv(require('./data/time-breakdown-all.csv'))
   .catch(err => console.log('Failed with', err))
 
 function ready(datapoints) {
-  let nested = d3.nest().key(d => d.project).entries(datapoints)
+  let projects = datapoints.map(d => d.project)
+  xPositionScale.domain(projects)
+
+  let nested = d3
+    .nest()
+    .key(d => d.project)
+    .entries(datapoints)
 
   svg
-    .selectAll('path')
-    .data(pie(nested))
+    .selectAll('.pie')
+    .data(nested)
     .enter()
-    .append('path')
-    .attr('d', d => arc(d))
-    .attr('fill', d => colorScale(d.data.task))
+    .append('g')
+    .attr('class', 'pie')
+    .attr('transform', d => `translate(${xPositionScale(d.key)},${height / 2})`)
+    .each(function(d) {
+      let g = d3.select(this)
 
+      g.selectAll('path')
+        .data(pie(d.values))
+        .enter()
+        .append('path')
+        .attr('d', d => arc(d))
+        .attr('fill', d => colorScale(d.data.task))
+
+      g.append('text')
+        .text(d.key)
+        .attr('text-anchor', 'middle')
+        .attr('alignment-baseline', 'top')
+        .attr('dy', height / 2 - 10)
+    })
 }
